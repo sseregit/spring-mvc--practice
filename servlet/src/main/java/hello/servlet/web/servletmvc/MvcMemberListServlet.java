b01083f2d01558b29a39fc99c53c9c410fe79012
yeonsig jang <yeonsigjang@yeonsigui-MacBookAir.local>
2022-11-09 00:03:56 +0900

    2022.11.09 00:03

---
+++ servlet/src/main/java/hello/servlet/web/servletmvc/MvcMemberListServlet.java
@@ -0,0 +1,36 @@
+package hello.servlet.web.servletmvc;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import hello.servlet.domain.Member;
+import hello.servlet.domain.MemberRepository;
+
+@WebServlet(name = "mvcMemberListServlet", urlPatterns = "/servlet-mvc/members")
+public class MvcMemberListServlet extends HttpServlet{
+	
+	private MemberRepository memberRepository = MemberRepository.getInstance();
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		List<Member> members = memberRepository.findAll();
+		
+		req.setAttribute("members", members);
+		
+		String viewPath = "/WEB-INF/views/members.jsp";
+		RequestDispatcher dispatcher = req.getRequestDispatcher(viewPath);
+		dispatcher.forward(req, resp);
+		
+	}
+
+	
+	
+}
---
+++ servlet/src/main/webapp/WEB-INF/views/save-result.jsp
@@ -0,0 +1,15 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <meta charset="UTF-8">
+</head>
+<body>
+ 성공
+  <ul>
+      <li>${member.id}</li>
+      <li>${member.username}</li>
+      <li>${member.age}</li>
+</ul>
+<a href="/index.html">메인</a>
+  </body>
+  </html>
